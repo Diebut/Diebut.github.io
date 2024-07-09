17d456b2baa12538cf4ccf0b98f8c6c5f09b7167
Diego Guevara <56057944+Diebut@users.noreply.github.com>
2024-07-09 16:13:44 -0300

    Actualizar registro.js

--- js/registro.js
+++ js/registro.js
@@ -8,10 +8,7 @@ document.querySelector("#registroForm").addEventListener("submit", (e) => {
   e.preventDefault();
   const name = document.querySelector("#name").value;
   const email = document.querySelector("#email").value;
-  const contacto = document.querySelector("#tel").value;/* agrega */
-  const pais = document.querySelector("#pais").select; /* agrego */
   const password = document.querySelector("#password").value;
-  const check = document.querySelector("#accept_terms").Checked; /* agrego */
   const Users = JSON.parse(localStorage.getItem("users")) || [];
   const isUserRegistered = Users.find((user) => user.email === email);
 
@@ -19,7 +16,7 @@ document.querySelector("#registroForm").addEventListener("submit", (e) => {
     return alert("El usuario ya está registrado!");
   }
 
-  Users.push({ name, email, contacto, pais, password, check });
+  Users.push({ name, email, password });
   localStorage.setItem("users", JSON.stringify(Users));
   alert("Registro Exitoso!");
   window.location.href = "index.html";
